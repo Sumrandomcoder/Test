e0fa925c4962c57981aa106add3f043e2aab711f
Christian Osbey <osbeychristian@gmail.com>
2023-10-30 14:41:03 -0700

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Osjk33 on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
